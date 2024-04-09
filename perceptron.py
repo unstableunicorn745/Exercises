f6b513756c7353c2588713dfb64e0ef192a6109d
unstableunicorn745 <130515173+unstableunicorn745@users.noreply.github.com>
2024-04-08 20:47:42 -0400

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -3,8 +3,17 @@ w_weights = [0.4, 0.3, 0.6]
 threshold = 0.5
 
 def step(weighted_sum):
-  if weighted_sum > threshold:
-    return 1
-  else:
-    return 0
+	if weighted_sum > threshold:
+		return 1
+	else:
+		return 0
 
+def perceptron():
+	weighted_sum = 0
+	for x,w in zip(x_input, w_weight):
+		weighted_sum += x*w
+		print(weighted_sum)
+	return step(weighted_sum)
+
+output = perceptron()
+print("output: " + str(output))
